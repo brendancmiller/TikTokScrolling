6a4e4ab272440978b22b254704079d492cf3eaeb
Brendan Miller <brendancmiller@gmail.com>
2026-01-13 11:39:47 -0500

    Initial Commit

---
+++ TikTokScrolling/TikTokScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokScrollingApp.swift
+//  TikTokScrolling
+//
+//  Created by Brendan Miller on 2026-01-13.
+//
+
+import SwiftUI
+
+@main
+struct TikTokScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
